5a4ad864b6b5c416498afeb871a5bf96174bfd20
Florian Knodt <git@adlerweb.info>
2019-03-28 20:11:17 +0100

    Allow for temperature compensated output

--- src/HCSR04.cpp
+++ src/HCSR04.cpp
@@ -15,6 +15,11 @@ UltraSonicDistanceSensor::UltraSonicDistanceSensor(
 }
 
 double UltraSonicDistanceSensor::measureDistanceCm() {
+    //Using the approximate formula 19.307°C results in roughly 343m/s which is the commonly used value for air.
+    return measureDistanceCm(19.307);
+}
+
+double UltraSonicDistanceSensor::measureDistanceCm(float temperature) {
     // Make sure that trigger pin is LOW.
     digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
@@ -24,7 +29,9 @@ double UltraSonicDistanceSensor::measureDistanceCm() {
     digitalWrite(triggerPin, LOW);
     // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
     unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
-    double distanceCm = durationMicroSec / 2.0 * 0.0343;
+
+    double speedOfSoundInCmPerMs = 0.03313 + 0.0000606 * temperature // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+    double distanceCm = durationMicroSec / 2.0 * speedOfSoundInCmPerMs;
     if (distanceCm == 0 || distanceCm > 400) {
         return -1.0 ;
     } else {
