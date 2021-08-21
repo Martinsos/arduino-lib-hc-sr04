c931650136e7fd6258bd6215d3407d9c71d1dce9
Nicolas T <nicolas@furyweb.fr>
2021-08-21 20:12:46 +0200

    feat: Added max distance and max time as constructor parameters.
    * timeout
    
    Add timeout to pulsein as HC-SR04 max range is about 4m.
    
    * fix_timeout
    
    Timeout doubled to allow echo returns
    
    * Add variable to set max distance
    
    Set max distance (default 4m for HC-SR04) for setting timeout to pulsein.
    
    * rename timeout variable
    
    Rename timeout to maxDistanceDurationMicroSec, fix pulseIn usage with this variable.
    
    * fix invalid distance check
    
    Hardcoded distance replaced by property maxDistanceCm
    
    * Fix maxDistance calculation
    
    * Add absolute timeout
    
    Add private property and method to set absolute timeout of pulseIn.
    
    * integration of max timeout to constructor
    
    * use both maxDistance & tiemout if set
    
    * rename timeoutMicroSec
    
    * type change
    
    * documentation
    
    * fix maxDistanceDurationMicroSec
    
    * adjust types and fix doc
    
    * more types change to reduce memory usage
    
    * add documentation
    
    * Polished comments a bit.
    
    Co-authored-by: Martin Šošić <Martinsos@users.noreply.github.com>

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ Sensor is initialized by creating instance of class UltraSonicDistanceSensor and
 UltraSonicDistanceSensor sensor(triggerPin, echoPin);
 ```
 
+Default value for maximum measurement distance is 4m, since HC-SR04 sensor can't measure reliably beyond that.
+However, if you are using another sensor or if you you don't care about distances larger than some value, you can set a maximum distance in cm yourself.
+```c
+UltraSonicDistanceSensor sensor(triggerPin, echoPin, maxDistanceCm);
+```
+
+Besides defining max distance, you can also define max time of measurement (in micro seconds). This is important when you want to ensure your program does not block for longer than specific period of time.
+```c
+UltraSonicDistanceSensor sensor(triggerPin, echoPin, maxDistanceCm, absoluteTimeout);
+```
+
 Then, to measure the distance, you just call `measureDistanceCm()`, which will return distance in centimeters (double). If distance is larger than 400cm, it will return negative value.
 
 The calculation assumes a temperature of around 20°C. For improved accuracy you may also provide a temperature yourself, either an average for your location or directly measured from another sensor. The call for a temperature of 3.5°C would as such look like this: `measureDistanceCm(3.5)`.
@@ -29,8 +40,8 @@ In this simple example, we need to connect sensors pins like this:
 #include <HCSR04.h>
 
 // Initialize sensor that uses digital pins 13 and 12.
-int triggerPin = 13;
-int echoPin = 12;
+const byte triggerPin = 13;
+const byte echoPin = 12;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
 void setup () {
@@ -39,7 +50,7 @@ void setup () {
 
 void loop () {
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
-    double distance = distanceSensor.measureDistanceCm();
+    float distance = distanceSensor.measureDistanceCm();
     Serial.println(distance);
     delay(500);
 }
--- src/HCSR04.h
+++ src/HCSR04.h
@@ -13,25 +13,35 @@ class UltraSonicDistanceSensor {
     /**
      * @param triggerPin  Digital pin that is used for controlling sensor (output).
      * @param echoPin  Digital pin that is used to get information from sensor (input).
+     * @param maxDistanceCm  Maximum distance sensor can measure, defaults to 4m for HC-SR04.
+     *                       You might want to set this value if you are using different sensor than HC-SR04
+     *                       or if you don't care about distances larger than whatever you will set it to
+     *                       (therefore reducing time it takes for a single measurement).
+     * @param maxTimeoutMicroSec  Single measurement will never take longer than whatever value you provide here.
+     *   You might want to do this in order to ensure your program is never blocked for longer than some specific time,
+     *   since measurements are blocking.
+     *   By default, there is no limit on time (only on distance). By defining timeout, you are again limiting the distance. 
      */
-    UltraSonicDistanceSensor(int triggerPin, int echoPin);
+    UltraSonicDistanceSensor(byte triggerPin, byte echoPin, unsigned short maxDistanceCm = 400, unsigned long maxTimeoutMicroSec = 0);
 
     /**
      * Measures distance by sending ultrasonic waves and measuring time it takes them
      * to return.
      * @returns Distance in centimeters, or negative value if distance is greater than 400cm.
      */
-    double measureDistanceCm();
+    float measureDistanceCm();
 
     /**
      * Measures distance by sending ultrasonic waves and measuring time it takes them
-     * to return.
+     * to return. Measurement can not exceed duration calculated with maxDistanceCm or maxTimeoutMicroSec.
      * @param temperature  Temperature in degrees celsius
      * @returns Distance in centimeters, or negative value if distance is greater than 400cm.
      */
-    double measureDistanceCm(float temperature);
+    float measureDistanceCm(float temperature);
  private:
-    int triggerPin, echoPin;
+    byte triggerPin, echoPin;
+    unsigned short maxDistanceCm;
+    unsigned long maxTimeoutMicroSec;
 };
 
 #endif // HCSR04_H
